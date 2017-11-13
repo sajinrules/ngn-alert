357ae6d8eefaf9c034e66289eed4af5b3bb467a9
sajin <sajina@qburst.com>
2017-11-13 17:57:17 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NgnAlertModule } from './modules/ngn-alert/ngn-alert.module';
+import { NgnAlertComponent } from './modules/ngn-alert/ngn-alert.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    NgnAlertModule,
     BrowserModule
   ],
   providers: [],
