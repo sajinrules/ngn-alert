efb05d8b4211587e438a7e1be5382eded3347fce
sajin <sajina@qburst.com>
2017-11-20 18:26:30 +0530

    design changes

--- src/app/modules/ngn-alert/ngn-alert.component.ts
+++ src/app/modules/ngn-alert/ngn-alert.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
 
 @Component({
-  selector: 'app-ngn-alert',
-  templateUrl: './ngn-alert.component.html',
-  styleUrls: ['./ngn-alert.component.css']
+	selector: 'app-ngn-alert',
+	templateUrl: './ngn-alert.component.html',
+	styleUrls: ['./ngn-alert.component.css']
 })
+
 export class NgnAlertComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+	@Input() options:any={};
+	constructor(){
+	}
+	ngOnInit(){
+		console.log("options:",this.options);
+	}
 
 }
