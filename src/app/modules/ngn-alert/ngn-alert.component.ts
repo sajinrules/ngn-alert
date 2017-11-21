16ecb5642cb056e4b471c8885c759c0018eec805
sajin <sajina@qburst.com>
2017-11-21 11:12:40 +0530

    option type tested

--- src/app/modules/ngn-alert/ngn-alert.component.ts
+++ src/app/modules/ngn-alert/ngn-alert.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit,Input } from '@angular/core';
-
+import { Options } from './ngn-interface'
 @Component({
 	selector: 'app-ngn-alert',
 	templateUrl: './ngn-alert.component.html',
@@ -7,7 +7,7 @@ import { Component, OnInit,Input } from '@angular/core';
 })
 
 export class NgnAlertComponent implements OnInit {
-	@Input() options:any={};
+	@Input() options:Options;
 	constructor(){
 	}
 	ngOnInit(){
